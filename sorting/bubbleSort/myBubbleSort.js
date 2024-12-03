166d06ed00751e39981faf55f4538e5e0c97d1c8
mohdasaadeh <mass.1993@hotmail.com>
2024-12-03 23:02:03 +0300

    improving the performance of the bubble sort algorithm

--- sorting/bubbleSort/myBubbleSort.js
+++ sorting/bubbleSort/myBubbleSort.js
@@ -1,10 +1,16 @@
 function myBubbleSort(arr) {
   for (let i = arr.length - 1; i >= 0; i--) {
+    let noSwap = true;
+
     for (let j = 0; j <= i; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+
+        noSwap = false;
       }
     }
+
+    if (noSwap) break;
   }
 
   return arr;
