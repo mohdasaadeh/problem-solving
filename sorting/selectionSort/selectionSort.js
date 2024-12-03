c17e05217509696ee037af9f8a1cbc6395ed9747
mohdasaadeh <mass.1993@hotmail.com>
2024-12-03 23:16:48 +0300

    adding the selection sort algorithm

---
+++ sorting/selectionSort/selectionSort.js
@@ -0,0 +1,19 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+
+  return arr;
+}
+
+console.log(selectionSort([2, 5, 10, 3, 2, 1]));
