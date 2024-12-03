4075ccaf2cb6859a05219013aa608296add8b0e9
mohdasaadeh <m.saadeh@wajeez.co>
2024-12-03 22:51:37 +0300

    working on the bubble sorting algorithm

---
+++ sorting/bubbleSort/myBubbleSort.js
@@ -0,0 +1,13 @@
+function myBubbleSort(arr) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    for (let j = 0; j <= i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(myBubbleSort([2, 5, 10, 3, 2, 1]));
